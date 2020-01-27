e84f958179f6b08bc9fd3a4bc6bd11257e24207e
emigiustogA <51482521+emigiusto@users.noreply.github.com>
2020-01-27 15:36:36 -0300

    asd

--- dba/scripts/Otros/matchups.sql
+++ dba/scripts/Otros/matchups.sql
@@ -1,1 +1,32 @@
-SELECT jugadorresultado.resultado, jugadorresultado.jugador_id, jugadorresultado.partido_id, jugador.nombre, jugador.apellido, jugador.foto, partidos.golesganador, partidos.golesperdedor, partidos.fecha FROM jugadorresultado JOIN jugador ON jugadorresultado.jugador_id = jugador.id JOIN partidos ON jugadorresultado.partido_id = partidos.id WHERE (jugador_id = "+p1id+" OR jugador_id = "+p1id+") AND jugadorresultado.partido_id IN (SELECT jugadorresultado.partido_id FROM jugadorresultado JOIN partidotorneo on jugadorresultado.partido_id = partidotorneo.partido_id JOIN torneos on partidotorneo.torneo_id = torneos.id WHERE jugador_id = "+p1id+" OR jugador_id = "+p2id+" AND torneos.tipotorneo = 'Geofobal' GROUP BY jugadorresultado.partido_id HAVING COUNT(jugadorresultado.partido_id) = 2) ORDER BY jugadorresultado.partido_id DESC , jugador_id
+set @p1 = 4;
+set @p2 = 18;
+SELECT 
+    jugadorresultado.resultado,
+    jugadorresultado.jugador_id,
+    jugadorresultado.partido_id,
+    jugador.nombre,
+    jugador.apellido,
+    jugador.foto,
+    partidos.golesganador,
+    partidos.golesperdedor,
+    partidos.fecha
+FROM
+    jugadorresultado
+        JOIN
+    jugador ON jugadorresultado.jugador_id = jugador.id
+        JOIN
+    partidos ON jugadorresultado.partido_id = partidos.id
+WHERE
+    (jugador_id = @p1 OR jugador_id = 54)
+        AND jugadorresultado.partido_id IN (SELECT 
+            jugadorresultado.partido_id
+        FROM
+            jugadorresultado
+		JOIN partidotorneo on jugadorresultado.partido_id = partidotorneo.partido_id
+        JOIN torneos on partidotorneo.torneo_id = torneos.id
+        WHERE
+            jugador_id = @p1 OR jugador_id = 54
+			AND torneos.tipotorneo = 'Geofobal'
+        GROUP BY jugadorresultado.partido_id
+        HAVING COUNT(jugadorresultado.partido_id) = 2)
+ORDER BY jugadorresultado.partido_id DESC , jugador_id
