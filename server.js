54e7946111396e6ace74a984598171ce8b54461b
emigiusto <51482521+emigiusto@users.noreply.github.com>
2020-02-26 20:09:11 -0300

    asd

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
 
 //app.get('/players',controller.traerPlayers);
 app.get('/posiciones/:id',controller.infoTablaPosiciones);
