f33c2fb462367898b3a486b9e752eccab26c1e73
emigiusto <51482521+emigiusto@users.noreply.github.com>
2020-02-27 12:30:29 -0300

    asd

--- dba/conexionbd.js
+++ dba/conexionbd.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var pool  = mysql.createPool({
   connectionLimit : 10000,
-  host: "us-cdbr-iron-east-05.cleardb.net",
+  host: "us-cdbr-iron-east-04.cleardb.net",
   user: "b4e2bfbdde73e1",
   password: "827b6c9cfe8b03e",
   database: "heroku_26a00e66485bd8a"
