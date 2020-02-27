2ce2e636c0e53a083f9234450be525a779171033
emigiusto <51482521+emigiusto@users.noreply.github.com>
2020-02-27 12:02:51 -0300

    asd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var controller = require('./controlador/controlador')
 var app = express();
 
 
-/*app.use(cors());*/
+app.use(cors());*/
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -15,12 +15,12 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-/*app.use(function(req, res, next) {
+app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
-})*/
+})
 
 app.get('/with-cors', cors(), (req, res, next) => {
   res.json({ msg: 'WHOAH with CORS it works!' })
