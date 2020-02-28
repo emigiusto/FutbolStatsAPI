6f4f9a83db2be88135d5e920554f78822c0bfc78
emigiusto <51482521+emigiusto@users.noreply.github.com>
2020-02-28 18:02:32 -0300

    asd

--- controlador/controlador.js
+++ controlador/controlador.js
@@ -70,7 +70,8 @@ function traerRachaPlayer(req, res) {
 function ultimos20Jugador(req, res) {
     var jugador = req.params.jugadorId;
 
-    var sql = "select jugador.id as 'jugadorId', jugador.nombre,jugador.apellido, (select IF(jugadorresultado.resultado=1, 'G', IF(jugadorresultado.resultado=0, 'P', 'E'))) as 'resultado', jugadorresultado.partido_id as 'partidoId', partidos.fecha, (select IF(jugadorresultado.resultado=1, partidos.golesganador, partidos.golesperdedor)) as 'golesConvertidos', (select IF(jugadorresultado.resultado=1, partidos.golesperdedor, partidos.golesganador)) as 'golesRecibidos', jugadorresultado.golesjugador as 'golesindividuales' from jugadorresultado join jugador on jugadorresultado.jugador_id = jugador.id join partidos on jugadorresultado.partido_id = partidos.id where jugador.id ="+ jugador +" order by jugadorresultado.partido_id desc limit 20;"
+    var sql = "select jugador.id as 'jugadorId', jugador.nombre,jugador.apellido, (select IF(jugadorresultado.resultado=1, 'G', IF(jugadorresultado.resultado=0, 'P', 'E'))) as 'resultado', jugadorresultado.partido_id as 'partidoId', partidos.fecha, (select IF(jugadorresultado.resultado=1, partidos.golesganador, partidos.golesperdedor)) as 'golesConvertidos', (select IF(jugadorresultado.resultado=1, partidos.golesperdedor, partidos.golesganador)) as 'golesRecibidos', jugadorresultado.golesjugador as 'golesindividuales' from jugadorresultado join jugador on jugadorresultado.jugador_id = jugador.id join partidos on jugadorresultado.partido_id = partidos.id join partidotorneo on partidos.id = partidotorneo.partido_id join torneos on partidotorneo.torneo_id = torneos.id where jugador.id ="+ jugador +" and torneos.tipotorneo = 'Geofobal' order by jugadorresultado.partido_id desc limit 20;"
+    console.log(sql)
     con.query(sql, function(error, results, fields) {
         if (error) {
             console.log("Hubo un error en la consulta", error.message);
