dcf11ca22db448b53ea808437c21381093654c1c
emigiusto <51482521+emigiusto@users.noreply.github.com>
2020-02-28 18:23:42 -0300

    asd

--- dba/scripts/Otros/ultimos20.sql
+++ dba/scripts/Otros/ultimos20.sql
@@ -1,15 +1,14 @@
-select jugador.id as 'jugadorId',
-jugador.nombre,jugador.apellido, 
-(select IF(jugadorresultado.resultado=1, 'W', IF(jugadorresultado.resultado=0, 'L', 'E'))) as 'resultado',
+select jugador.id as 'jugadorId', jugador.nombre,jugador.apellido, 
+(select IF(jugadorresultado.resultado=1, 'G', IF(jugadorresultado.resultado=0, 'P', 'E'))) as 'resultado', 
 jugadorresultado.partido_id as 'partidoId', 
-partidos.fecha, 
-(select IF(jugadorresultado.resultado=1, partidos.golesganador, partidos.golesperdedor)) as 'golesConvertidos',
-(select IF(jugadorresultado.resultado=1, partidos.golesperdedor, partidos.golesganador)) as 'golesRecibidos',
-jugadorresultado.golesjugador as 'golesindividuales'
+partidos.fecha, (select IF(jugadorresultado.resultado=1, partidos.golesganador, partidos.golesperdedor)) as 'golesConvertidos', 
+(select IF(jugadorresultado.resultado=1, partidos.golesperdedor, partidos.golesganador)) as 'golesRecibidos', 
+jugadorresultado.golesjugador as 'golesindividuales' 
+
 from jugadorresultado 
+
 join jugador on jugadorresultado.jugador_id = jugador.id 
 join partidos on jugadorresultado.partido_id = partidos.id 
-
-where jugador.id =54 
-
+join partidotorneo on partidos.id = partidotorneo.partido_id join torneos on partidotorneo.torneo_id = torneos.id
+where jugador.id =54 and torneos.tipotorneo = 'Geofobal'
 order by jugadorresultado.partido_id desc limit 20;
